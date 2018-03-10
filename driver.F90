1e8aebe533f71db5d65d232146c5627e23d150b5
David Appelhans <dappelh@us.ibm.com>
2018-03-10 10:36:30 -0500

    many stream working, but double buffer seg fault

--- driver.F90
+++ driver.F90
@@ -7,7 +7,9 @@ program main
   integer, parameter :: nx=6*1024, ny=6*1024, nz = 36
   complex, pinned, allocatable :: a(:,:,:), b(:,:,:)
 
-  real, dimension(3) :: res, exp
+  integer :: ierr
+
+  real, dimension(3) :: res
 
   allocate( a(nx,ny,nz),b(nx,ny,nz) )
 
@@ -23,5 +25,18 @@ program main
   res(1) = maxval(abs(a-b/(nx*ny)))
   write(*,"(A,G8.3)") 'Max error C2C: ', res(1)
   print *,'Max error C2C: ', res(1)
+
+  a = 1
+
+  call  cufft2dTest_2stream(a,b,nx,ny,nz)
+
+  ierr = cudaDeviceSynchronize()
+
+  res(1) = maxval(abs(a-b/(nx*ny)))
+  write(*,"(A,G8.3)") 'Max error C2C: ', res(1)
+  print *,'Max error C2C: ', res(1)
+
+
+
 
 end program main
--- overlapfft_mod.F90
+++ overlapfft_mod.F90
@@ -51,6 +51,10 @@ contains
      ierr = cudaMemcpyAsync(b(:,:,z), d_b(:,:,s), nx*ny, mystream(s))
   enddo
 
+  ierr = cudaDeviceSynchronize()
+
+  deallocate(d_a)
+  deallocate(d_b)
   
 end subroutine cufft2dTest_manystream
 
@@ -63,44 +67,58 @@ subroutine cufft2dTest_2stream(a,b,nx,ny,nz)
   complex, device, allocatable :: d_a(:,:,:), d_b(:,:,:)
   integer, allocatable :: plan(:)
   integer :: ierr
-  integer :: z, s, nstreams
-  !  integer(kind=cuda_stream_kind) :: transfer, compute
-  integer(kind=cuda_stream_kind), allocatable :: mystream(:)
-  type(cudaEvent), allocatable :: HtoDcomplete(:), DtoHcomplete(:)
+  integer :: z, s, nbuffers
+  integer(kind=cuda_stream_kind) :: transfer_stream, compute_stream
+  type(cudaEvent), allocatable :: HtoDcomplete(:), DtoHcomplete(:), fft_complete(:)
 
-  nstreams = 3
-  allocate( mystream(nstreams) )
-  allocate( HtoDcomplete(nstreams) )
-  allocate( DtoHcomplete(nstreams) )
+  nbuffers = 2 ! double buffer
+
+  allocate( HtoDcomplete(nbuffers) )
+  allocate( DtoHcomplete(nbuffers) )
   
-  allocate( plan(nstreams) )
+  allocate( plan(nbuffers) )
 
-  do s = 1,nstreams
-     ierr = cudaStreamCreate( mystream(s) )
+  ierr = cudaStreamCreate(transfer_stream)
+  ierr = cudaStreamCreate(compute_stream)
+
+  do s = 1,nbuffers
+     !ierr = cudaStreamCreate( mystream(s) )
      ierr = cudaEventCreate(HtoDcomplete(s))
      ierr = cudaEventCreate(DtoHcomplete(s))
+     ierr = cudaEventCreate(fft_complete(s))
      !mystream(s) = cudaforGetDefaultStream()
   enddo
 
   
-  allocate( d_a(nx,ny,nstreams), d_b(nx,ny,nstreams) )
+  allocate( d_a(nx,ny,nbuffers), d_b(nx,ny,nbuffers) )
 
 
-  do s = 1,nstreams
-     ! set as many plans as I have streams
+  do s = 1,nbuffers
+     ! set as many plans as I have buffers
      ierr = cufftPlan2D(plan(s),ny,nx,CUFFT_C2C)
-     ierr = cufftSetStream(plan(s),mystream(s))
+     ierr = cufftSetStream(plan(s),compute_stream)
   enddo
 
   do z=1,nz
-     s = mod(z,nstreams) + 1
-     ierr = cudaMemcpyAsync(d_a(:,:,s),a(:,:,z),nx*ny,mystream(s))
-     ierr = cudaEventRecord(HtoDcomplete(s),mystream(s))
+     s = mod(z,nbuffers) + 1
+     ierr = cudaMemcpyAsync(d_a(:,:,s),a(:,:,z),nx*ny,transfer_stream)
+     ierr = cudaEventRecord(HtoDcomplete(s),transfer_stream)
 
+     ! compute stream waits for HtoD to complete in transfer stream 
+     ierr = cudaStreamWaitEvent(compute_stream, HtoDcomplete(s),0)
      ierr = ierr + cufftExecC2C(plan(s),d_a(:,:,s),d_b(:,:,s),CUFFT_FORWARD)
      ierr = ierr + cufftExecC2C(plan(s),d_b(:,:,s),d_b(:,:,s),CUFFT_INVERSE)
-     ierr = cudaMemcpyAsync(b(:,:,z), d_b(:,:,s), nx*ny, mystream(s))
+     ierr = cudaEventRecord(fft_complete(s),compute_stream)
+
+     ! transfer_stream waits until compute is finished
+     ierr = cudaStreamWaitEvent(transfer_stream, fft_complete(s),0)
+     ierr = cudaMemcpyAsync(b(:,:,z), d_b(:,:,s), nx*ny, transfer_stream)
   enddo
+
+  ierr = cudaDeviceSynchronize()
+
+  deallocate(d_a)
+  deallocate(d_b)
 
 
 end subroutine cufft2dTest_2stream
